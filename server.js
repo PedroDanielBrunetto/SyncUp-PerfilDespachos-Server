555b6c5ae47264ee6dfcdf48a6cbf70ac3042f73
pedrodanielbrunetto <pedrodanielbm@hotmail.com>
2024-11-28 16:00:44 -0300

    feat: CORS

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const SendMailService = require("./services/SendMailService");
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 app.post("/send-email", async (req, res) => {
